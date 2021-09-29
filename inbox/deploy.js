2f4c56531ab37313a117609ea52a426e7b4565f7
soyjoseroberto <jrodriguez18@gmail.com>
2021-09-28 21:56:51 -0400

    Added deploy.js code with HDWallerProvider.

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,11 @@
 // deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'bronze then gasp carbon wagon weapon infant fetch ready mountain scheme absent',
+    'https://rinkeby.infura.io/v3/75e2902f956341cbaad5e73251c1902e'
+);
+
+const web3 = new Web3(provider);
